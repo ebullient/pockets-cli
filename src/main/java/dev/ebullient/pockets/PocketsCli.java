002335e0c9ae0d457c8681a85b7ac1a1d74e2bee
Erin Schnabel <erinschnabel@gmail.com>
2021-12-08 22:15:14 -0500

    CLI tests

--- src/main/java/dev/ebullient/pockets/PocketsCli.java
+++ src/main/java/dev/ebullient/pockets/PocketsCli.java
@@ -1,19 +1,21 @@
 package dev.ebullient.pockets;
 
+import java.util.concurrent.Callable;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "greeting", mixinStandardHelpOptions = true)
-public class GreetingCommand implements Runnable {
+@Command(name = "pockets", mixinStandardHelpOptions = true)
+public class PocketsCli implements Callable<Integer> {
 
     @Parameters(paramLabel = "<name>", defaultValue = "picocli",
         description = "Your name.")
     String name;
 
     @Override
-    public void run() {
+    public Integer call() {
         System.out.printf("Hello %s, go go commando!\n", name);
+        return CommandLine.ExitCode.OK; // <--- Return USAGE, test will fail
     }
-
 }
---
+++ src/test/java/dev/ebullient/pockets/PocketsCliTest.java
@@ -0,0 +1,36 @@
+package dev.ebullient.pockets;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.main.Launch;
+import io.quarkus.test.junit.main.LaunchResult;
+import io.quarkus.test.junit.main.QuarkusMainLauncher;
+import io.quarkus.test.junit.main.QuarkusMainTest;
+
+@QuarkusMainTest
+class PocketsCliTest {
+
+    @Test
+    @Launch({})
+    public void testBasicCommand(LaunchResult result) {
+    }
+
+    @Test
+    public void testBasicCommandHelp(QuarkusMainLauncher launcher) {
+        // Aliases should emit the same output
+        LaunchResult result = launcher.launch("--help");
+        LaunchResult result2 = launcher.launch("-h");
+        Assertions.assertEquals(0, result.exitCode());
+        Assertions.assertEquals(result.getOutput(), result2.getOutput());
+    }
+
+    @Test
+    public void testBasicCommandVersion(QuarkusMainLauncher launcher) {
+        // Aliases should emit the same output
+        LaunchResult result = launcher.launch("--version");
+        LaunchResult result2 = launcher.launch("-V");
+        Assertions.assertEquals(0, result.exitCode());
+        Assertions.assertEquals(result.getOutput(), result2.getOutput());
+    }
+}
