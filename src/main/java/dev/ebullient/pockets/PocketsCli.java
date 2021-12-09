88eb6d21b6d7a2f22207a65560b215faf08bb079
Erin Schnabel <erinschnabel@gmail.com>
2021-12-08 22:36:53 -0500

    nice logs

--- src/main/java/dev/ebullient/pockets/PocketsCli.java
+++ src/main/java/dev/ebullient/pockets/PocketsCli.java
@@ -2,20 +2,46 @@ package dev.ebullient.pockets;
 
 import java.util.concurrent.Callable;
 
+import javax.inject.Inject;
+
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Parameters;
+import picocli.CommandLine.IFactory;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.ParseResult;
+import picocli.CommandLine.ScopeType;
+import picocli.CommandLine.Spec;
+import picocli.CommandLine.Model.CommandSpec;
 
+@QuarkusMain
 @Command(name = "pockets", mixinStandardHelpOptions = true)
-public class PocketsCli implements Callable<Integer> {
+public class PocketsCli implements Callable<Integer>, QuarkusApplication {
 
-    @Parameters(paramLabel = "<name>", defaultValue = "picocli",
-        description = "Your name.")
-    String name;
+    @Inject
+    IFactory factory;
+
+    @Option(names = { "--verbose", "-v" }, description = "verbose output", scope = ScopeType.INHERIT)
+    void setVerbose(boolean verbose) {
+        Log.setVerbose(verbose);
+    }
 
     @Override
     public Integer call() {
-        System.out.printf("Hello %s, go go commando!\n", name);
-        return CommandLine.ExitCode.OK; // <--- Return USAGE, test will fail
+        return CommandLine.ExitCode.OK;
+    }
+
+    private int executionStrategy(ParseResult parseResult) {
+        // Initialize log streams, carry on with the rest of the show
+        Log.prepareStreams(parseResult.commandSpec());
+        return new CommandLine.RunLast().execute(parseResult);
+    }
+
+    @Override
+    public int run(String... args) throws Exception {
+        return new CommandLine(this, factory)
+            .setExecutionStrategy(this::executionStrategy)
+            .execute(args);
     }
 }
